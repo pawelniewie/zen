e5369f6906354913ba0e0cfe287fd8e59fd61eed
Pawel Niewiadomski <11110000b@gmail.com>
2016-01-16 21:14:55 +0100

    added IssuesListView with some issues

--- src/views/IssuesListView/IssuesListView.js
+++ src/views/IssuesListView/IssuesListView.js
@@ -2,9 +2,7 @@ import React from 'react'
 import { connect } from 'react-redux'
 import { Link } from 'react-router'
 import { actions as counterActions } from '../../redux/modules/counter'
-import styles from './ProjectView.scss'
-import FooterView from 'views/FooterView/FooterView'
-import IssuesListView from 'views/IssuesListView/IssuesListView'
+import styles from './IssuesListView.scss'
 
 // We define mapStateToProps where we'd normally use
 // the @connect decorator so the data requirements are clear upfront, but then
@@ -14,7 +12,7 @@ import IssuesListView from 'views/IssuesListView/IssuesListView'
 const mapStateToProps = (state) => ({
   counter: state.counter
 })
-export class ProjectView extends React.Component {
+export class IssueView extends React.Component {
   static propTypes = {
     counter: React.PropTypes.number.isRequired,
     doubleAsync: React.PropTypes.func.isRequired,
@@ -23,23 +21,19 @@ export class ProjectView extends React.Component {
 
   render () {
     return (
-      <div className='container'>
-        <div className="page-header">
-          <h1>
-          <span className="fa-stack">
-            <i className="fa fa-circle fa-stack-2x"></i>
-            <i className="fa fa-rocket fa-stack-1x fa-inverse"></i>
-          </span>
-          {this.props.params.projectKey}
-          </h1>
-        </div>
-
-        <IssuesListView/>
-
-        <FooterView/>
+      <div>
+      <h4>Open issues:</h4>
+<ul className="list-group">
+  <li className="list-group-item">Add project name</li>
+  <li className="list-group-item">Add issue icons</li>
+  <li className="list-group-item">Each issue should be a link to issue view</li>
+  <li className="list-group-item">On issue list show issue key</li>
+  <li className="list-group-item">Add recently completed issues</li>
+  <li className="list-group-item">Show a list of team members in the project</li>
+</ul>
       </div>
     )
   }
 }
 
-export default connect(mapStateToProps, counterActions)(ProjectView)
+export default connect(mapStateToProps, counterActions)(IssueView)
